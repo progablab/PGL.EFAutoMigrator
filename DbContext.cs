d9882880a5377cea0c7b14eb5415e1edd63c80ac
Bruno <g@progablab.com>
2020-07-06 16:17:35 -0700

    Code supporting the concept

---
+++ DbContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace PGL.EFCFExtensions
+{
+    [DbConfigurationType(typeof(ModelConfiguration))]
+    internal class DbContext : System.Data.Entity.DbContext
+    {
+        public DbContext()
+        {
+            Configuration.LazyLoadingEnabled = true;
+            Configuration.ProxyCreationEnabled = true;
+            Configuration.AutoDetectChangesEnabled = true;
+            Configuration.ValidateOnSaveEnabled = true;
+        }
+
+        /// <summary>
+        ///     Adds all ModelBase sub-classed types as tables to the SQL database. Sets DateTime properties to use
+        ///     datetime2 SQL data type columns. Removes all pluralization from sql object naming. Disables EF proxies. Auto
+        ///     migrates SQL objects when there ModelBase source definitions are changed, does not allow changes to incur
+        ///     data-loss unless DDD.App.LinkCheck was a debug build.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            foreach (Type _Type in AppDomain.CurrentDomain.GetAssemblies()
+                                            .SelectMany(a => a.GetTypes()
+                                                              .Where(t => !t.IsAbstract && t.IsSubclassOf(typeof(ModelBase)))))
+                modelBuilder.RegisterEntityType(_Type);
+
+            //The .Net DateTime datatype may have values outside the SQL DateTime data type, SQL datetime2 encompasses all .Net DateTime data types
+            modelBuilder.Properties<DateTime>().Configure(c => c.HasColumnType("datetime2"));
+
+            //TODO:Set some string properties to be a fixed width so column compression can be applied to them
+
+            // reflection is used heavily throughout the solution, names must remain the same between models, poco & sql statements
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>();
+            modelBuilder.Conventions.Remove<TableAttributeConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private class ModelConfiguration : DbConfiguration
+        {
+            public ModelConfiguration()
+            {
+                SetDatabaseInitializer(new MigrateDatabaseToLatestVersion<DbContext, SqlDbMigrationsConfiguration<DbContext>>(true));
+            }
+        }
+
+        /// <summary>
+        ///     assigns the DocTypeName to the primary table & it's child table's Entity Framework Code First
+        ///     __MigrationHistory.ContextKey
+        /// </summary>
+        private class SqlDbMigrationsConfiguration<TContext> : DbMigrationsConfiguration<TContext>
+            where TContext : System.Data.Entity.DbContext
+        {
+            public SqlDbMigrationsConfiguration()
+            {
+                AutomaticMigrationsEnabled = true;
+                AutomaticMigrationDataLossAllowed = false;
+            }
+        }
+    }
+}
---
+++ ModelBase.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Reflection;
+
+namespace PGL.EFCFExtensions
+{
+    /// <summary>
+    ///     Allows for recursive saves of entities to the database. Saves include insert & updates of existing objects based on
+    ///     the <see cref="ModelBase.Id" />. These extension methods only work with composite type properties; not fields. Base
+    ///     type all business objects (data vestals) should stem from.
+    /// </summary>
+    public abstract class ModelBase
+    {
+        public int Id { get; set; }
+
+        internal ModelBase attachedEntity(System.Data.Entity.DbContext dbContext)
+        {
+            return set(dbContext)
+                .Local.AsQueryable()
+                .Cast<ModelBase>()
+                .FirstOrDefault(m => m.Id == Id);
+        }
+
+        internal DbEntityEntry entry(System.Data.Entity.DbContext dbContext)
+        {
+            return dbContext.Entry(this);
+        }
+
+        /// <summary>
+        ///     attaches the objects graph & it's child objects via navigation properties back the the DBContext & executes
+        ///     Adds/Updates. This object graph attacher works specifically with ModelBase & generic lists of them
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="dbContext"></param>
+        /// <param name="autoSaveChanges">
+        ///     should a <see cref="System.Data.Entity.DbContext.SaveChanges" /> call be issued to persist this to the
+        ///     database? When no; you must call the <see cref="System.Data.Entity.DbContext.SaveChanges" /> manually
+        /// </param>
+        public void Save(System.Data.Entity.DbContext dbContext, bool autoSaveChanges = true)
+        {
+            if (Id == default(int))
+            {
+                set(dbContext)
+                    .Add(this);
+            }
+            else
+            {
+                if (entry(dbContext)
+                    .State == EntityState.Detached)
+                    if (attachedEntity(dbContext) != null)
+                        dbContext.Entry(attachedEntity(dbContext))
+                            .CurrentValues.SetValues(this);
+                    else
+                        entry(dbContext)
+                            .State = EntityState.Modified;
+            }
+
+            foreach (PropertyInfo _PropertyInfo in GetType()
+                .GetProperties())
+                if (_PropertyInfo.GetValue(this,
+                    null) != null)
+                    if (_PropertyInfo.PropertyType.IsSubclassOf(typeof(ModelBase)))
+                        ((ModelBase) _PropertyInfo.GetValue(this,
+                            null)).Save(dbContext,
+                            false);
+                    else if (_PropertyInfo.PropertyType.GetInterface("IList") != null)
+                        foreach (ModelBase _LinkCheckInfoBase in ((IList) _PropertyInfo.GetValue(this,
+                            null)).OfType<ModelBase>())
+                            _LinkCheckInfoBase.Save(dbContext,
+                                false);
+
+
+            if (autoSaveChanges)
+                dbContext.SaveChanges();
+        }
+
+        internal DbSet set(System.Data.Entity.DbContext dbContext)
+        {
+            return dbContext.Set(GetType());
+        }
+    }
+}
